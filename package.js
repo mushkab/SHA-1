6b5e58a74b035c01ddace352e9c52712fc4f9a7d
mush <moshe.kabalo@gmail.com>
2015-09-20 12:54:47 +0300

    add meteor packaging to SHA-1

---
+++ package.js
@@ -0,0 +1,27 @@
+// package metadata file for Meteor.js
+var packageName = 'mushk:sha1';
+var where = ''; // where to install: 'client' or 'server'. For both, pass nothing.
+var version = '0.1.1';
+var summary = 'This is a SHA-1 hash generator by JavaScript';
+var gitLink = 'https://github.com/mushkab/SHA-1.git';
+var documentationFile = 'README.md';
+
+// Meta-data
+Package.describe({
+  name: packageName,
+  version: version,
+  summary: summary,
+  git: gitLink,
+  documentation: documentationFile
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']); // Meteor versions
+
+  
+
+  api.addFiles('sha1.js', where);
+ 
+
+  // Files in use
+});  
